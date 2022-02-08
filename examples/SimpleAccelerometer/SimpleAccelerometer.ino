8c4cafac9a7a732e54b5c4f1573ba0a50cc7ae2c
Main Push Robot <robot@example.com>
2022-02-08 08:18:14 +0000

    Committing dev changes

--- examples/SimpleAccelerometer/SimpleAccelerometer.ino
+++ examples/SimpleAccelerometer/SimpleAccelerometer.ino
@@ -20,14 +20,14 @@ void setup() {
   while (!Serial);
   Serial.println("Started");
 
-  if (!LSM9.begin()) 
+  if (!LSM9.begin()) //Check if communication is started successfully
   {
     Serial.println("Failed to initialize LSM9!");
     while (1);
   }
 
   Serial.print("Accelerometer sample rate = ");
-  Serial.print(LSM9.accelerationSampleRate());
+  Serial.print(LSM9.accelerationSampleRate()); //Get sample rate
   Serial.println(" Hz");
   Serial.println();
   Serial.println("Acceleration in G's");
@@ -37,14 +37,15 @@ void setup() {
 void loop() {
   float x, y, z;
 
-  if (LSM9.accelerationAvailable()) 
+  if (LSM9.accelerationAvailable()) //Check if sensor has measured acceleration
   {
-    LSM9.readAcceleration(x, y, z);
+    LSM9.readAcceleration(x, y, z); //Get x, y and z axis acceleration and
+                                    //save them in variables
 
-    Serial.print(x);
+    Serial.print(x);                //Print x axis acceleration
     Serial.print('\t');
-    Serial.print(y);
+    Serial.print(y);                //Print y axis acceleration
     Serial.print('\t');
-    Serial.println(z);
+    Serial.println(z);              //Print z axis acceleration
   }
 }
