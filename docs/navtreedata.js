32eeb1b5e6c6a7be3283d54816c0800daf3dc1ae
Docs Robot <robot@example.com>
2023-02-14 15:21:08 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,9 +25,7 @@
 var NAVTREE =
 [
   [ "LSM9DS1 Accelerometer, Gyroscope and Magnetometer library", "index.html", [
-    [ "Accelerometer & Gyroscope & Magnetometer LSM9DS1TR breakout Arduino library", "index.html", [
-      [ "Have fun!", "index.html#autotoc_md8", null ]
-    ] ],
+    [ "Accelerometer & Gyroscope & Magnetometer LSM9DS1TR breakout Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
